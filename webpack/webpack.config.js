c1aa42d66b3832735642940c11023cf1b8afc99b
Andrei Bachinin <limbus727@gmail.com>
2024-08-15 21:34:08 -0400

    Webpack config is updated. Merger is used.

---
+++ webpack/webpack.config.js
@@ -0,0 +1,8 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
+
+module.exports = (envVars) => {
+    const { env } = envVars; //переменную env мы будем передавать при запуске скрипта со значением dev или prod
+    const envConfig = require(`./webpack.${env}.js`)
+    return merge(commonConfig, envConfig)
+}
